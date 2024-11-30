abf633576c42d5386c9717159cd7dbeeb61e53d6
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2024-11-30 12:41:10 +0100

    Added ViaJarHubBackend

---
+++ ViaJarHubBackend/src/main/java/it/unical/demacs/informatica/viajarhubbackend/ViaJarHubBackendApplication.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.informatica.viajarhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaJarHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViaJarHubBackendApplication.class, args);
+    }
+
+}
