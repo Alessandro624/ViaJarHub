87e8cda2efd65be7321b04488aeb7376e061df5e
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2024-12-06 23:14:36 +0100

    Fixed range slider and added some custom color and behaviour

--- AngularTestApp/src/app/filtri/filtri.component.ts
+++ AngularTestApp/src/app/filtri/filtri.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, Validators} from '@angular/forms';
-import {CommonModule, DatePipe, NgIf, NgStyle} from '@angular/common';
-import {MatSliderModule} from '@angular/material/slider';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CommonModule, NgIf} from '@angular/common';
 
-import {ChangeContext, LabelType, NgxSliderModule, Options} from '@angular-slider/ngx-slider';
-
+import {LabelType, NgxSliderModule, Options} from '@angular-slider/ngx-slider';
 
 
 @Component({
@@ -13,34 +11,40 @@ import {ChangeContext, LabelType, NgxSliderModule, Options} from '@angular-slide
   imports: [
     CommonModule,
     FormsModule,
-    DatePipe,
-    NgStyle,
     NgIf,
     NgxSliderModule
   ],
   templateUrl: './filtri.component.html',
   styleUrl: './filtri.component.css'
 })
-export class FiltriComponent  {
-
+export class FiltriComponent {
   isExpanded: boolean = false; // Proprietà per mostrare/nascondere il pannello
   minValue: number = 100; // Valore minimo iniziale
   maxValue: number = 400; // Valore massimo iniziale
-
   options: Options = {
     floor: 0,
     ceil: 500,
+    noSwitching: true,
+    showTicksValues: true,
+    tickStep: 50,
+    tickValueStep: 50,
+    getSelectionBarColor: (value: number) => {
+      return 'black'
+    },
+    getPointerColor: (value: number) => {
+      return 'red'
+    },
+    getTickColor: (value: number) => {
+      return 'green'
+    },
     translate: (value: number, label: LabelType): string => {
       switch (label) {
-
         case LabelType.Low:
-          console.log(value)
-          return `<b>Min price:</b> $${value}`;
+          return '<b>Min price:</b> €' + value;
         case LabelType.High:
-          console.log(LabelType.High)
-          return `<b>Max price:</b> $${value}`;
+          return '<b>Max price:</b> €' + value;
         default:
-          return `$${value}`;
+          return '€' + value;
       }
     }
   };
@@ -69,7 +73,7 @@ export class FiltriComponent  {
 
 
   // Utility per formattare la data in "YYYY-MM-DD" per HTML5
-   formatDate(date: Date): string {
+  formatDate(date: Date): string {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Mesi da 0 a 11
     const year = date.getFullYear();
@@ -79,18 +83,6 @@ export class FiltriComponent  {
   changeFormat(string: string): string {
     const [year, month, day] = string.split('-');
     return `${day}-${month}-${year}`;
-  }
-
-  // Verifica e corregge i valori quando l'utente termina l'interazione
-  onUserChangeEnd(changeContext: ChangeContext): void {
-    // Assicura che il valore minimo non superi il massimo
-    if (this.minValue > this.maxValue) {
-      this.minValue = this.maxValue;
-    }
-    // Assicura che il valore massimo non scenda sotto il minimo
-    if (this.maxValue < this.minValue) {
-      this.maxValue = this.minValue;
-    }
   }
 
   filters = {
@@ -110,16 +102,7 @@ export class FiltriComponent  {
     this.isExpanded = !this.isExpanded; // Cambia lo stato tra true e false
   }
 
-  adjustRange() {
-    // Correggi il range nel caso min superi max
-    if (this.filters.priceRange.min > this.filters.priceRange.max) {
-      const temp = this.filters.priceRange.min;
-      this.filters.priceRange.min = this.filters.priceRange.max;
-      this.filters.priceRange.max = temp;
-    }
-  }
-
-  // Per testare l'applicazione dei filtri
+// Per testare l'applicazione dei filtri
   applyFilters(): void {
     console.log('Filtri applicati:', this.filters);
   }
