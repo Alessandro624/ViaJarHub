f7d772ea5eb41544d2b5d47aa0162221fe5ade45
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2024-12-23 13:11:48 +0100

    Added travel table into ViaJarHub database

--- ViaJarHubBackend/src/main/java/it/unical/demacs/informatica/viajarhubbackend/controller/MapsController.java
+++ ViaJarHubBackend/src/main/java/it/unical/demacs/informatica/viajarhubbackend/controller/MapsController.java
@@ -3,6 +3,7 @@ package it.unical.demacs.informatica.viajarhubbackend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
@@ -12,31 +13,31 @@ import java.net.URL;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/open/v1")
 public class MapsController {
 
-        private String apiKey="AIzaSyB_MjRfLP2aXTTxyDxQ64QxkPnQhKqzB4A";
+    private String apiKey = "AIzaSyB_MjRfLP2aXTTxyDxQ64QxkPnQhKqzB4A";
 
-        @RequestMapping("/open/v1/map")
-        public ResponseEntity<String> getMap(@RequestBody Map<String, Double> coordinates) throws Exception {
-            double lat = coordinates.get("latitude");
-            double lng = coordinates.get("longitude");
-            System.out.println(lat + " " + lng);
-            String urlStr = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=" + apiKey;
-            URL url = new URL(urlStr);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
+    @RequestMapping(value = "/map", method = RequestMethod.POST)
+    public ResponseEntity<String> getMap(@RequestBody Map<String, Double> coordinates) throws Exception {
+        double lat = coordinates.get("latitude");
+        double lng = coordinates.get("longitude");
+        System.out.println(lat + " " + lng);
+        String urlStr = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=" + apiKey;
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-            conn.disconnect();
-            System.out.println(content.toString());
-            return ResponseEntity.ok(content.toString());
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
         }
+        in.close();
+        conn.disconnect();
+        System.out.println(content.toString());
+        return ResponseEntity.ok(content.toString());
     }
+}
 
