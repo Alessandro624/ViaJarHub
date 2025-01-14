0e822d1663966eada450f7d175ad96313264c41a
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-01-14 11:14:21 +0100

    Minor changes to PaymentController

--- ViaJarHubBackend/src/main/java/it/unical/demacs/informatica/viajarhubbackend/controller/booking/PaymentController.java
+++ ViaJarHubBackend/src/main/java/it/unical/demacs/informatica/viajarhubbackend/controller/booking/PaymentController.java
@@ -7,6 +7,7 @@ import com.stripe.param.PaymentIntentConfirmParams;
 import com.stripe.param.PaymentIntentCreateParams;
 import it.unical.demacs.informatica.viajarhubbackend.config.security.SecurityUtility;
 import it.unical.demacs.informatica.viajarhubbackend.model.Booking;
+import it.unical.demacs.informatica.viajarhubbackend.model.Travel;
 import it.unical.demacs.informatica.viajarhubbackend.persistence.DAO.implJDBC.proxy.UserProxy;
 import it.unical.demacs.informatica.viajarhubbackend.service.IEmailService;
 import it.unical.demacs.informatica.viajarhubbackend.service.ITravelService;
@@ -18,6 +19,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 @RestController
@@ -33,7 +36,6 @@ public class PaymentController {
 
     @PostConstruct
     public void init() {
-        // Imposta la chiave segreta di Stripe
         Stripe.apiKey = System.getenv("STRIPE_VIAJARHUB");
     }
 
@@ -44,106 +46,102 @@ public class PaymentController {
 
     @RequestMapping(value = "/payment", method = RequestMethod.POST)
     public ResponseEntity<Map<String, Object>> processPayment(@RequestBody Map<String, Object> paymentData) {
-
-
         Map<String, Object> response = new HashMap<>();
         try {
-            // Validazione dei dati in ingresso
             if (!checkNotNullPaymentData(paymentData, response) || !validatePaymentData(paymentData, response)) {
                 return ResponseEntity.badRequest().body(response);
             }
-            // Conversione dell'importo in centesimi
             long amount = Math.round(((Number) paymentData.get("amount")).doubleValue() * 100);
-            // Creazione del PaymentIntent
             PaymentIntentCreateParams createParams = PaymentIntentCreateParams.builder()
                     .setAmount(amount)
                     .setCurrency("eur")
                     .addPaymentMethodType("card")
                     .build();
             PaymentIntent paymentIntent = PaymentIntent.create(createParams);
-            // Conferma del pagamento con un metodo di test
             PaymentIntentConfirmParams confirmParams = PaymentIntentConfirmParams.builder()
-                    .setPaymentMethod("pm_card_visa") // Metodo di pagamento di test
+                    .setPaymentMethod("pm_card_visa")
                     .build();
             paymentIntent = paymentIntent.confirm(confirmParams);
             Booking booking = new Booking((Integer) paymentData.get("numeroPartecipanti"), ((Number) paymentData.get("amount")).doubleValue(), LocalDateTime.now());
             this.addToBooking(Long.valueOf((Integer) paymentData.get("travelId")), booking);
-            // Risposta di successo
             response.put("success", true);
             response.put("status", paymentIntent.getStatus());
-            System.out.println(response);
             this.sendEmail(paymentData);
             return ResponseEntity.ok(response);
         } catch (StripeException e) {
-            // Risposta di errore Stripe
             response.put("success", false);
             response.put("message", "Errore durante il pagamento: " + e.getMessage());
-            System.out.println(response);
             return ResponseEntity.badRequest().body(response);
         } catch (Exception e) {
-            // Risposta di errore generico
             response.put("success", false);
             response.put("message", "Errore generico: " + e.getMessage());
-            e.printStackTrace();
-            System.out.println(response);
             return ResponseEntity.badRequest().body(response);
         }
     }
 
     private boolean validatePaymentData(Map<String, Object> paymentData, Map<String, Object> response) {
-        // TODO check amount = travel.price * participantsNumber
-        // TODO check email = current user.email
-        // TODO check travel existence
-        // TODO check date
         Long travelId = Long.valueOf((Integer) paymentData.get("travelId"));
+        Optional<Travel> travel = travelService.findById(travelId, false);
+        if (travel.isEmpty()) {
+            response.put("success", false);
+            response.put("message", "Viaggio non trovato.");
+            return false;
+        }
+        String email = (String) paymentData.get("email");
+        if (!Objects.equals(email, SecurityUtility.getCurrentUser().getUsername())) {
+            response.put("success", false);
+            response.put("message", "Email non valida.");
+            return false;
+        }
+        Double amount = ((Number) paymentData.get("amount")).doubleValue();
         int numberOfParticipants = (Integer) paymentData.get("numeroPartecipanti");
+        double price = travel.get().getPrice();
+        if (!Objects.equals(amount, price * numberOfParticipants)) {
+            response.put("success", false);
+            response.put("message", "Importo non valido.");
+            return false;
+        }
         LocalDate startDate = LocalDate.parse(paymentData.get("dataPartenza").toString());
+        if (!Objects.equals(startDate, travel.get().getStartDate())) {
+            response.put("success", false);
+            response.put("message", "Data di partenza non valida.");
+            return false;
+        }
         LocalDate endDate = LocalDate.parse(paymentData.get("dataRitorno").toString());
-        System.out.println("travelId " + travelId);
-        System.out.println("participantsNumber " + numberOfParticipants);
-        System.out.println("startDate " + startDate);
-        System.out.println("endDate " + endDate);
-        // Controlla la disponibilità dei posti
+        if (!Objects.equals(endDate, travel.get().getEndDate())) {
+            response.put("success", false);
+            response.put("message", "Data di ritorno non valida.");
+            return false;
+        }
         int availableSeats = travelService.getAvailableSeats(travelId, startDate, endDate);
-        System.out.println("availableSeats " + availableSeats);
         if (numberOfParticipants > availableSeats) {
             response.put("success", false);
             response.put("message", "Non ci sono abbastanza posti disponibili.");
-            System.out.println(response);
             return false;
         }
-        // Controllo del formato del numero di carta
         String cardNumber = paymentData.get("cardNumber").toString().replaceAll(" ", "");
         if (!isValidCardNumber(cardNumber)) {
-            System.out.println("sss");
             response.put("success", false);
             response.put("message", "Numero di carta non valido.");
-            System.out.println(response);
             return false;
         }
-        // Controllo del mese di scadenza
         int expiryMonth = (Integer) paymentData.get("expiryMonth");
         if (expiryMonth < 1 || expiryMonth > 12) {
             response.put("success", false);
             response.put("message", "Mese di scadenza non valido.");
-            System.out.println(response);
             return false;
         }
-        // Controllo dell'anno di scadenza
         int expiryYear = (Integer) paymentData.get("expiryYear");
         int currentYear = java.time.Year.now().getValue();
-        if (expiryYear < currentYear || expiryYear > currentYear + 20) { // Limite massimo di 20 anni nel futuro
+        if (expiryYear < currentYear || expiryYear > currentYear + 20) {
             response.put("success", false);
             response.put("message", "Anno di scadenza non valido.");
-            System.out.println(response);
             return false;
         }
-        // Controllo del formato del CVV
         String cvv = (String) paymentData.get("cvv");
         if (!isValidCVV(cvv)) {
             response.put("success", false);
             response.put("message", "CVV non valido.");
-            System.out.println(response);
             return false;
         }
         return true;
@@ -153,71 +151,59 @@ public class PaymentController {
         if (!paymentData.containsKey("travelId") || paymentData.get("travelId") == null) {
             response.put("success", false);
             response.put("message", "Viaggio mancante o non valido.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("email") || paymentData.get("email") == null) {
             response.put("success", false);
             response.put("message", "Email mancante o non valida.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("numeroPartecipanti") || paymentData.get("numeroPartecipanti") == null) {
             response.put("success", false);
             response.put("message", "Numero partecipanti mancante o non valido.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("dataPartenza") || paymentData.get("dataPartenza") == null) {
             response.put("success", false);
             response.put("message", "Data partenza mancante o non valido.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("dataRitorno") || paymentData.get("dataRitorno") == null) {
             response.put("success", false);
             response.put("message", "Data ritorno mancante o non valido.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("amount") || paymentData.get("amount") == null) {
             response.put("success", false);
             response.put("message", "Importo mancante o non valido.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("cardNumber") || paymentData.get("cardNumber") == null) {
             response.put("success", false);
             response.put("message", "Numero di carta mancante.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("expiryMonth") || paymentData.get("expiryMonth") == null) {
             response.put("success", false);
             response.put("message", "Mese di scadenza mancante.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("expiryYear") || paymentData.get("expiryYear") == null) {
             response.put("success", false);
             response.put("message", "Anno di scadenza mancante.");
-            System.out.println(response);
             return false;
         }
         if (!paymentData.containsKey("cvv") || paymentData.get("cvv") == null) {
             response.put("success", false);
             response.put("message", "CVV mancante.");
-            System.out.println(response);
             return false;
         }
         return true;
     }
 
     private boolean isValidCardNumber(String cardNumber) {
-        // Controllo base per il numero di carta (Luhn Algorithm)
         Pattern pattern = Pattern.compile("^\\d{16}$");
         if (!pattern.matcher(cardNumber).matches()) {
-            System.out.println("a");
             return false;
         }
         return luhnCheck(cardNumber);
@@ -241,7 +227,6 @@ public class PaymentController {
     }
 
     private boolean isValidCVV(String cvv) {
-        // CVV deve essere 3-4 cifre
         Pattern pattern = Pattern.compile("^\\d{3,4}$");
         return pattern.matcher(cvv).matches();
     }
